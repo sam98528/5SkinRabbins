b00160435d86c4f5619bd8f2f6ff41ac591dfc6b
Hee48 <lvovl7070@naver.com>
2024-04-01 17:30:49 +0900

    test

--- 5SkinRabbins/ViewController/ViewController.swift
+++ 5SkinRabbins/ViewController/ViewController.swift
@@ -4,11 +4,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
     
     let test : String = "123"
-
 }
 
-//git 나쁜......
+
