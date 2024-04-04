8f7b062b63dce47ac4e30e6f1cae2cbc2858473d
Seungwon Lee <sam98528@naver.com>
2024-04-04 12:58:06 +0900

    Revert "Seungwon"

--- 5SkinRabbins/SceneDelegate.swift
+++ 5SkinRabbins/SceneDelegate.swift
@@ -16,15 +16,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        
-        window!.rootViewController = UIStoryboard(name: "DetailsView", bundle: nil).instantiateInitialViewController()
-        /*
-        for fontFamily in UIFont.familyNames {
-            for fontName in UIFont.fontNames(forFamilyName: fontFamily) {
-                print(fontName)
-            }
-        }
-         */
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
